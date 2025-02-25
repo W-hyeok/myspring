46f47c6f070c0e167d6e4cade57ab6ac3baca456
hyeok <hyeok5049@gmail.com>
2025-02-25 11:19:04 +0900

    Spring boot war project myspring upload 1

---
+++ src/main/java/com/ex01/myspiring/MyspiringApplication.java
@@ -0,0 +1,13 @@
+package com.ex01.myspiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex01/myspiring/MyspiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex01.myspiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
